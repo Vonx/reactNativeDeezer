6c6d0e198a696bb8a2784728212cb4b314e2a513
t1 <ryanthonn@gmail.com>
2018-10-13 23:36:50 -0700

    added loading bars between searches

--- reactNativeDoodle/screens/AlbumScreen1.js
+++ reactNativeDoodle/screens/AlbumScreen1.js
@@ -5,6 +5,7 @@ import { ExpoLinksView } from '@expo/samples';
 import {CardList} from '../components/cardList';
 import * as actions from '../actions';
 import { SearchText } from '../components/SearchText';
+import { Bars } from 'react-native-loader';
 
 export default class AlbumScreen1 extends React.Component {
 
@@ -15,29 +16,47 @@ export default class AlbumScreen1 extends React.Component {
     constructor() {
         super();
         this.state = {
-            searchText: 'eminem',
-            albums: null
+            searchText: '',
+            albums: [],
+            isFetching: false
         };
 
-     this.inputArtist();
+     this.inputArtist = this.inputArtist.bind(this);
+
     }
 
     inputArtist () {
-        actions.searchTracks(this.state.searchText).then((albums) => {
-            this.setState({albums: albums})
-        })
+        this.setState({isFetching: true, albums: []});
+
+        actions.searchTracks(this.state.searchText)
+            .then((albums) => {this.setState({albums: albums, isFetching: false})})
+            .catch(err => this.setState({albums: [], isFetching: false}))
+    }
+
+    renderAlbumsView () {
+        return (
+        <ScrollView style={styles.container}>
+            <FormLabel containerStyle={styles.center}>Search an Artist</FormLabel>
+            <FormInput onChangeText={(event) => {
+                this.setState({searchText: event})
+            }}/>
+            <Button title='Search' onPress={() => {
+                this.inputArtist()
+            }}/>
+            {this.state.albums.length > 0 && !this.state.isFetching &&
+            <CardList data={this.state.albums} imageKey={'cover_big'} titleKey={'title'} buttonText={'See more details'}
+                      tracklist={'tracklist'}/>
+            }
+            {this.state.albums.length === 0 && this.state.isFetching &&
+                <Bars style={styles.center} size={20} color="#000000" />
+            }
+        </ScrollView>
+        )
     }
 
     render() {
     // debugger;
-        return (
-            <ScrollView style={styles.container}>
-                <FormLabel containerStyle={styles.center}>Search an Artist</FormLabel>
-                <FormInput onChangeText={(event)=>{this.setState({searchText: event})}}/>
-                <Button title='search' onPress={()=>{this.inputArtist()}}/>
-                <CardList data = {this.state.albums} imageKey={'cover_big'} titleKey={'title'} buttonText={'See more details'} tracklist={'tracklist'}/>
-            </ScrollView>
-        );
+        return this.renderAlbumsView();
     }
 }
 
@@ -46,5 +65,9 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 15,
         backgroundColor: '#fff',
+    },
+    center: {
+        alignSelf: 'center',
+        alignItems: 'center'
     },
 });
